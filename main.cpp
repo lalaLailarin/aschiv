b78d794bd633a76006072ab84293bcbaeb66a1d0
Elise Douglas <162293649+lalaLailarin@users.noreply.github.com>
2024-03-05 10:27:41 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+  float nearest_dist = 1e10;
+    if (std::abs(dir.y)>.001) { // intersect the ray with the checkerboard, avoid division by zero
+        float d = -(orig.y+4)/dir.y; // the checkerboard plane has equation y = -4
+        vec3 p = orig + dir*d;
+        if (d>.001 && d<nearest_dist && std::abs(p.x)<10 && p.z<-10 && p.z>-30) {
+            nearest_dist = d;
+            pt = p;
+            N = {0,1,0};
+            material.diffuse_color = (int(.5*pt.x+1000) + int(.5*pt.z)) & 1 ? vec3{.3, .3, .3} : vec3{.3, .2, .1};
+        }
+    }
+
+   float diffuse_light_intensity = 0, specular_light_intensity = 0;
+    for (const vec3 &light : lights) { // checking if the point lies in the shadow of the light
+        vec3 light_dir = (light - point).normalized();
+        auto [hit, shadow_pt, trashnrm, trashmat] = scene_intersect(point, light_dir);
+        if (hit && (shadow_pt-point).norm() < (light-point).norm()) continue;
+        diffuse_light_intensity  += std::max(0.f, light_dir*N);
+        specular_light_intensity += std::pow(std::max(0.f, -reflect(-light_dir, N)*dir), material.specular_exponent);
+    }
